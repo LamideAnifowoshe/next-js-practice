af1fa45fb367a5784c7d58c13ad72f9f7a6db9ac
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-07 06:13:13 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,12 @@ import headerStyles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div>
-      <h1>
+      <h1 className={headerStyles.title}>
         <span>WebDev</span> News
       </h1>
-      <style jsx>
-        {`
-          .title {
-            color: red;
-          }
-        `}
-      </style>
+      <p className={headerStyles.description}>
+        Keep up to date with web dev news
+      </p>
     </div>
   );
 };
